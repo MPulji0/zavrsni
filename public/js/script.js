4e2c35769543a8e8479b8f1993e19b1cc30b4911
MPulji0 <puljiz939@gmail.com>
2019-07-15 13:50:22 +0200

    Dodan izbornik na Uploadaj tab

--- public/js/script.js
+++ public/js/script.js
@@ -13,12 +13,6 @@
 //   window.location.href = '/'
 // }
 
-function closeBtnFunc() {
-  const loginPanelDiv = document.querySelector('.loginPanel')
-  console.log(loginPanelDiv)
-  loginPanelDiv.style.display = "none"
-}
-
 function loginPanelFunc() {
   const loginPanelDiv = document.querySelector('.loginPanel')
   loginPanelDiv.style.display = "block"
@@ -31,20 +25,6 @@ function signUpPanelFunc() {
   signupPanelDiv.style.display = "block"
   const loginPanelDiv = document.querySelector('.loginPanel')
   loginPanelDiv.style.display = "none"
-}
-
-function pocetnaMenu() {
-  window.location.href = '/'
-}
-
-function filmoviMenu() {
-  window.location.href = "/filmovi"
-  const filmoviTab = document.getElementById('filmovi').parentNode.className="active"
-  console.log(filmoviTab)
-}
-
-function uploadMenu() {
-  window.location.href = "/upload"
 }
 
 function activeTabNavBar(val) {
@@ -148,37 +128,6 @@ async function main() {
       });
     });
   });
-
-  let cookieValues = document.cookie
-  let arr = cookieValues.split(';')
-  let result = arr[0].search('uname')
-
-  if (result > -1) {
-    const loginDiv = document.querySelector('.login-div')
-    let username = arr[0].substr(arr[0].search('=') + 1)
-
-    loginDiv.style.display = "none"
-    const signoutDiv = document.querySelector('.signout-div')
-    const signoutBtn = document.querySelector('#signoutBtn')
-
-    signoutBtn.value = username
-    signoutDiv.style.display = "block"
-  }
-
-  if (result === -1) {
-    const loginDiv = document.querySelector('.login-div')
-    loginDiv.style.display = "block"
-    const signoutDiv = document.querySelector('.signout-div')
-    signoutDiv.style.display = "none"
-  }
-
-  if (window.location.href === 'http://localhost:8080/') {
-    activeTabNavBar(0)
-  } else if (window.location.href === 'http://localhost:8080/filmovi') {
-    activeTabNavBar(1)
-  } else if (window.location.href === 'http://localhost:8080/upload') {
-    activeTabNavBar(2)
-  }
 
   await buildSlideShowCards()
 }
--- public/js/utils.js
+++ public/js/utils.js
@@ -10,3 +10,66 @@ async function getMovies(url) {
     const requestRes = await fetch(url)
     return await requestRes.json()
 }
+
+function pocetnaMenu() {
+    window.location.href = '/'
+}
+  
+function filmoviMenu() {
+    window.location.href = "/filmovi"
+    const filmoviTab = document.getElementById('filmovi').parentNode.className="active"
+    console.log(filmoviTab)
+}
+  
+function uploadMenu() {
+    window.location.href = "/upload"
+}
+
+function activeTabNavBar(val) {
+    const navBarList = document.querySelector('.navbar-nav')
+    const childrenArr = navBarList.children
+    for(let i = 0; i < childrenArr.length; ++i){
+      childrenArr[i].className = ''
+    }
+    childrenArr[val].className = 'active'
+}
+
+function closeBtnFunc() {
+    const loginPanelDiv = document.querySelector('.loginPanel')
+    const signupPanelDiv = document.querySelector('.signupPanel')
+    loginPanelDiv.style.display = "none"
+    signupPanelDiv.style.display = "none"
+}
+
+
+// MAIN
+if (window.location.href === 'http://localhost:8080/') {
+    activeTabNavBar(0)
+} else if (window.location.href === 'http://localhost:8080/filmovi') {
+    activeTabNavBar(1)
+} else if (window.location.href === 'http://localhost:8080/upload') {
+    activeTabNavBar(2)
+}
+
+let cookieValues = document.cookie
+let arr = cookieValues.split(';')
+let result = arr[0].search('uname')
+
+if (result > -1) {
+  const loginDiv = document.querySelector('.login-div')
+  let username = arr[0].substr(arr[0].search('=') + 1)
+
+  loginDiv.style.display = "none"
+  const signoutDiv = document.querySelector('.signout-div')
+  const signoutBtn = document.querySelector('#signoutBtn')
+
+  signoutBtn.value = username
+  signoutDiv.style.display = "block"
+}
+
+if (result === -1) {
+  const loginDiv = document.querySelector('.login-div')
+  loginDiv.style.display = "block"
+  const signoutDiv = document.querySelector('.signout-div')
+  signoutDiv.style.display = "none"
+}
